d466863ce7523cf8ccf10a76c1fe5dde676fa61f
Nurmuhammad032 <nurmuhammad2003o@gmail.com>
2023-11-22 16:47:34 +0500

    done

--- src/components/Settings/DateBirthday/DatePickerPage.jsx
+++ src/components/Settings/DateBirthday/DatePickerPage.jsx
@@ -25,7 +25,7 @@ const DatePickerPage = () => {
       <input
         datepicker
         type="text"
-        className="bg-gray-50 border border-gray-300  py-3 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300  py-3 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
         placeholder="Select date"
         // />
         // {/* <input */}
