5a73fbef79d17eef10715c2b7d38e94e68c297af
Nurmuhammad032 <nurmuhammad2003o@gmail.com>
2023-11-22 16:43:35 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "./Layout/AuthPage/Login";
-import Register from "./Layout/AuthPage/Register";
-import Continue from "./Layout/AuthPage/Continue";
-import CreateAnAcc from "./Layout/AuthPage/CreateAnAcc";
-import ConfirmPassword from "./Layout/AuthPage/ConfirmPassword";
-import CheckEmail from "./Layout/AuthPage/CheckEmail";
-import ForgotPassword from "./Layout/AuthPage/ForgotPassword";
-import Congratulation from "./Layout/AuthPage/Congratulation";
-import Main from "./components/Main";
 import './App.css'
-import About from "./components/About";
-import Contact from "./components/Contact";
 import Settings from './components/Settings/DateBirthday/Settings'
 import Billings from "./components/Settings/Billings/Billings";
 
